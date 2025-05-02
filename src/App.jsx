4b229fd5e0c46a97c9283368fd3313272d198299
mhbigham <mhbigham@hotmail.co.uk>
2025-05-02 01:41:44 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#1e3a8a' }}>TrustRise Capital</h1>
+      <p>Welcome to our investment platform. Site is live and powered by Vite + React!</p>
+    </main>
+  );
+}
